1f0e856ee4509f10bb18069993af3c3af884394e
RemiViez <remi.viez@etu.univ-lehavre.fr>
2025-10-08 14:07:19 +0000

    correction condition en double sur arg

--- Q5.java
+++ Q5.java
@@ -5,9 +5,6 @@ public class Q5 {
             return;
         }
         int N = Integer.parseInt(args[0]);
-        if (args.length > 0) {
-            N = Integer.parseInt(args[0]);
-        }
         for (int i = N; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
